8a9bf36b678b7939aa73c9f926a1b9cb1cf475fb
rra3060 <rnrjsah3066@gmail.com>
2025-04-25 15:54:34 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn test() -> impl Responder {
-  HttpResponse::Ok().body("test")
+    HttpResponse::Ok().body("test")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
 fn main(){
