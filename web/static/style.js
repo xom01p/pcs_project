6ba38b6c7361ce671490521cbe9a8da84537cbf6
rra3060 <rnrjsah3066@gmail.com>
2025-04-26 22:01:34 +0900

    Update style.js

--- web/static/style.js
+++ web/static/style.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
+    var height = window.innerHeight,
+        x = 0,
+        y = height / 2,
+        curveX = 10,
+        curveY = 0,
+        targetX = 0,
+        xitteration = 0,
+        yitteration = 0,
+        menuExpanded = false;
+
+    var blob = $('#blob'),
+        blobPath = $('#blob-path'),
+        hamburger = $('.hamburger');
+
+    console.log('jQuery loaded:', typeof $ !== 'undefined');
+    console.log('Blob element:', blob.length);
 }
